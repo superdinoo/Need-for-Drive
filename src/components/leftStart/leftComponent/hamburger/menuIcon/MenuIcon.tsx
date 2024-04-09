ff6f30ac342bff2b94c7a4b020d1db52dc1bcdab
Your Name <serzh.bakalov.81@mail.ru>
2024-04-09 22:07:28 +0400

    Готовая верстка v-1

--- src/components/leftStart/leftComponent/hamburger/menuIcon/MenuIcon.tsx
+++ src/components/leftStart/leftComponent/hamburger/menuIcon/MenuIcon.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { RxHamburgerMenu, RxCross2 } from 'react-icons/rx'
+import * as iconRx from 'react-icons/rx'
 
 interface MenuIconsProps {
-  name: string
+  name: keyof typeof iconRx
   handleClick: () => void
 }
 
 const MenuIcon: React.FC<MenuIconsProps> = ({ name, handleClick }) => {
   const icons: { [key: string]: JSX.Element } = {
-    menu: <RxHamburgerMenu className="menuIcon" onClick={handleClick} />,
-    cross: <RxCross2 className="menuIcon" onClick={handleClick} />,
+    menu: <iconRx.RxHamburgerMenu className="menuIcon" onClick={handleClick} />,
+    cross: <iconRx.RxCross2 className="menuIcon" onClick={handleClick} />,
   }
   const ChosenIcon = icons[name]
 
