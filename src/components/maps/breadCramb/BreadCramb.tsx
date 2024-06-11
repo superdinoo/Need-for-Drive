904c63584477d298fb28c2400e22154b6d746dba
Your Name <serzh.bakalov.81@mail.ru>
2024-06-11 17:28:45 +0400

    WIP: f2

--- src/components/maps/breadCramb/BreadCramb.tsx
+++ src/components/maps/breadCramb/BreadCramb.tsx
@@ -1,30 +1,16 @@
 import React from 'react'
 import './BreadCramb.scss'
 import { IoMdArrowDropright } from 'react-icons/io'
-import { Link, useLocation } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import { selectActiveCar } from '../../cars/selectors'
-import { selectLocation } from '../order/selectorsOrder'
 import breadMap from './dataMapLink'
 import { selectModalTotal } from '../../totalPath/selectorsModalTotal'
+import useBreadcrumbHelpers from './helpersBreadCramb'
 
 const BreadCramb: React.FC = () => {
   const { confirm } = useSelector(selectModalTotal)
-  const { city, point } = useSelector(selectLocation)
-  const activeCar = useSelector(selectActiveCar)
-  const location = useLocation()
 
-  const cityAndPoint = city.length > 0 && point.length > 0
-
-  const getLinkTo = (crumb: { to: string }) => {
-    if (crumb.to === '/ModelCar' && !cityAndPoint) return '/LocationPage'
-    if (
-      crumb.to === '/Additionally' &&
-      !(cityAndPoint && activeCar.name.length >= 0)
-    )
-      return '/ModelCar'
-    return crumb.to
-  }
+  const { getLinkTo, colorClass } = useBreadcrumbHelpers(breadMap)
 
   return (
     <div className="breadCramb">
@@ -34,18 +20,10 @@ const BreadCramb: React.FC = () => {
             <p className="oilName">Заказ номер RU58491823</p>
           )}
           {!confirm &&
-            breadMap.map((crumbs) => (
+            breadMap.map((crumbs, index) => (
               <React.Fragment key={crumbs.to}>
                 <Link to={getLinkTo(crumbs)}>
-                  <p
-                    className="breadIcon"
-                    style={{
-                      color:
-                        location.pathname === crumbs.to ? '#0EC261' : 'black',
-                    }}
-                  >
-                    {crumbs.text}
-                  </p>
+                  <p className={colorClass(index)}>{crumbs.text}</p>
                 </Link>
                 {crumbs.id < breadMap.length && (
                   <IoMdArrowDropright color="gray" />
